b1b44d73763c294028321f2339ee1a397c8dbfb4
Zachary <zderish@gmail.com>
2023-11-10 15:20:06 -0500

    Add initial message and command recognition

--- server.py
+++ server.py
@@ -1,5 +1,11 @@
 import socket, json, random, datetime, hashlib, sys
 
+def handle_get():
+    return "in get"
+
+def handle_post():
+    return "in post"
+
 def start_server(IP, PORT):
 
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
@@ -10,6 +16,24 @@ def start_server(IP, PORT):
             conn, addr = s.accept()
             HTTPRequest = conn.recv(1024).decode("ascii")
             print(HTTPRequest)
+
+            lineOne = HTTPRequest.split("\r\n")[0]
+            lineOne = lineOne.split(" ")
+            HTTPCommand = lineOne[0]
+            RequestTarget = lineOne[1]
+            HTTPVersion = lineOne[2]
+
+            if HTTPCommand == "POST" and RequestTarget == "/":
+                print(handle_post())
+            elif HTTPCommand == "GET":
+                print(handle_get())
+            else:
+                #send “501 Not Implemented”
+                s.close()
+                exit()
+
+            s.close()
+            exit()
 
 def main():
     IP = sys.argv[1]
